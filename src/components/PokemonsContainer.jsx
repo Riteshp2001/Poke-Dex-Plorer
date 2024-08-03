e12b420b9c91f309142f1146a99257bcdf8d45b8
Riteshp2001 <riteshp2001@gmail.com>
2024-08-04 00:16:30 +0530

    Added Search pokemon Functionality and Css

--- src/components/PokemonsContainer.jsx
+++ src/components/PokemonsContainer.jsx
@@ -1,15 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Autocomplete, TextField, Chip } from '@mui/material';
 import usePokemons from '../hooks/usePokemons';
 import PokemonCard from './PokemonCard';
 
 const PokemonsContainer = ({ type }) => {
-    const pokemons = usePokemons(type);
-    
-    return (
-        <div className='pokemons-container'>
-            { pokemons.map((pokemon) => <PokemonCard key={ pokemon.id } pokemon={ pokemon } />) }
-        </div>
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemons, setSelectedPokemons] = useState([]);
+
+  const allPokemons = usePokemons(type);
+
+  useEffect(() => {
+    setPokemons(allPokemons);
+    setSelectedPokemons([]);
+  }, [allPokemons, type]);
+
+  const availablePokemons = pokemons.filter(
+    pokemon => !selectedPokemons.some(selected => selected.id === pokemon.id)
+  );
+
+  const handlePokémonChange = (event, newValue) => {
+    const uniquePokemons = Array.from(
+      new Map(newValue.map(pokemon => [pokemon.id, pokemon])).values()
     );
+    setSelectedPokemons(uniquePokemons);
+  };
+
+  // Define color based on type
+  const typeColors = {
+    bug: '#8cb230',
+    dark: '#58575f',
+    dragon: '#0f6ac0',
+    electric: '#eed535',
+    fairy: '#ed6ec7',
+    fighting: '#d04164',
+    fire: '#fd7d24',
+    flying: '#748fc9',
+    ghost: '#556aae',
+    grass: '#62b957',
+    ground: '#dd7748',
+    ice: '#61cec0',
+    normal: '#9da0aa',
+    poison: '#a552cc',
+    psychic: '#ea5d60',
+    rock: '#baab82',
+    steel: '#417d9a',
+    water: '#4a90da',
+  };
+
+  const dropdownStyle = {
+    backgroundColor: type ? typeColors[type] : '#fff', // Background color based on type
+  };
+
+  return (
+    <div className='pokemons-container-wrapper'>
+      <Autocomplete
+        multiple
+        options={availablePokemons.map(pokemon => ({
+          label: pokemon.name,
+          id: pokemon.id,
+        }))}
+        getOptionLabel={(option) => option.label}
+        value={selectedPokemons}
+        onChange={handlePokémonChange}
+        renderInput={(params) => <TextField {...params} label='Select Pokémon' />}
+        className='pokemon-dropdown'
+        style={dropdownStyle} // Apply dynamic style
+        renderTags={(tagValue, getTagProps) =>
+          tagValue.map((option, index) => (
+            <Chip
+              label={option.label}
+              {...getTagProps({ index })}
+              className='pokemon-chip'
+            />
+          ))
+        }
+      />
+
+      <div className='pokemons-container'
+        style={{ width: selectedPokemons.length > 0 ? '100%' : 'auto' }}>
+        {selectedPokemons.length > 0 ? (
+          selectedPokemons.map(pokemon => (
+            <PokemonCard key={pokemon.id} pokemon={pokemons.find(p => p.id === pokemon.id)} />
+          ))
+        ) : (
+          pokemons.length > 0 ? (
+            pokemons.map(pokemon => (
+              <PokemonCard key={pokemon.id} pokemon={pokemon} />
+            ))
+          ) : (
+            <p>Loading Pokémon...</p>
+          )
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default PokemonsContainer;
--- src/components/TypesBar.jsx
+++ src/components/TypesBar.jsx
@@ -1,15 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useTypes from '../hooks/useTypes';
 import { getTypeIconSrc } from '../utils/pokemon-helper';
 
 const TypesBar = ({ toggleType }) => {
     const types = useTypes();
-    const [selectedType, setSelectedType] = useState(null);
-
-    const handleTypeClick = (name) => {
-        setSelectedType(name);
-        toggleType(name);
-    };
 
     return (
         <nav className='types-bar'>
@@ -18,15 +12,13 @@ const TypesBar = ({ toggleType }) => {
                     const typeImg = getTypeIconSrc(name);
 
                     return (
-                        name !== "stellar" && (
-                            <a
-                                key={name}
-                                className={`${name} ${selectedType === name ? 'selected' : ''}`}
-                                onClick={() => handleTypeClick(name)}
-                            >
-                                <img src={typeImg} alt={name} />
-                            </a>
-                        )
+                        <a
+                            key={ name }
+                            className={ name }
+                            onClick={ () => toggleType(name) }
+                        >
+                            <img src={ typeImg } alt={ name } />
+                        </a>
                     );
                 })
             }
