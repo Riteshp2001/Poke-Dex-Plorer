4074f151f989a77db5d5c06847ba3fa2e3f8dddf
Riteshp2001 <riteshp2001@gmail.com>
2024-08-04 22:22:03 +0530

    updated usePokemon hook

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -2,68 +2,46 @@ import { useQuery } from "@tanstack/react-query";
 import { apiFetch } from "../utils/api-fetch";
 import { formatPokemonData } from "../utils/pokemon-helper";
 
-const pokemonCache = {
-    all: null,
-    individual: {}
-};
-
-const fetchAllPokemons = async () => {
-    const typesResponse = await apiFetch('/type');
-    const allTypes = typesResponse.results.map(type => type.name);
-
-    let allPokemonsArray = [];
-    for (let i = 0; i < allTypes.length; i++) {
-        const { pokemon: pokemonList } = await apiFetch(`/type/${allTypes[i]}`);
-        const pokemonsOfType = await Promise.all(
-            pokemonList.map(async ({ pokemon }) => {
-                const res = await fetch(pokemon.url);
-                const data = await res.json();
-                return formatPokemonData(data);
-            })
-        );
-        allPokemonsArray = allPokemonsArray.concat(pokemonsOfType);
-    }
-
-    // Remove duplicates by Pokémon ID
-    const uniquePokemons = Array.from(
-        new Map(allPokemonsArray.map(pokemon => [pokemon.id, pokemon])).values()
-    );
-
-    return uniquePokemons;
-};
-
-const fetchPokemonsByType = async (type) => {
-    const { pokemon: pokemonList } = await apiFetch(`/type/${type}`);
-    const pokemons = await Promise.all(
-        pokemonList.map(async ({ pokemon }) => {
-            const res = await fetch(pokemon.url);
-            const data = await res.json();
-            return formatPokemonData(data);
-        })
-    );
-
-    return pokemons;
-};
-
 const usePokemons = (type) => {
     const fetchPokemons = async () => {
         if (type === 'all') {
-            if (!pokemonCache.all) {
-                pokemonCache.all = await fetchAllPokemons();
+            const typesResponse = await apiFetch('/type');
+            const allTypes = typesResponse.results.map(type => type.name);
+
+            let allPokemonsArray = [];
+            for (let i = 0; i < allTypes.length; i++) {
+                const { pokemon: pokemonList } = await apiFetch(`/type/${allTypes[i]}`);
+                const pokemonsOfType = await Promise.all(
+                    pokemonList.map(async ({ pokemon }) => {
+                        const res = await fetch(pokemon.url);
+                        const data = await res.json();
+                        return formatPokemonData(data);
+                    })
+                );
+                allPokemonsArray = allPokemonsArray.concat(pokemonsOfType);
             }
-            return pokemonCache.all;
+
+            // Remove duplicates by Pokémon ID
+            const uniquePokemons = Array.from(
+                new Map(allPokemonsArray.map(pokemon => [pokemon.id, pokemon])).values()
+            );
+
+            return uniquePokemons;
         } else {
-            if (!pokemonCache.individual[type]) {
-                pokemonCache.individual[type] = await fetchPokemonsByType(type);
-            }
-            return pokemonCache.individual[type];
+            const { pokemon: pokemonList } = await apiFetch(`/type/${type}`);
+            const pokemons = await Promise.all(
+                pokemonList.map(async ({ pokemon }) => {
+                    const res = await fetch(pokemon.url);
+                    const data = await res.json();
+                    return formatPokemonData(data);
+                })
+            );
+
+            return pokemons;
         }
     };
 
-    const { data } = useQuery(['pokemons', type], fetchPokemons, {
-        staleTime: 1000 * 60 * 60, // Cache data for 1 hour
-        cacheTime: 1000 * 60 * 60 * 24, // Keep cache for 24 hours
-    });
+    const { data } = useQuery(['pokemons', type], fetchPokemons);
 
     return data;
 };
